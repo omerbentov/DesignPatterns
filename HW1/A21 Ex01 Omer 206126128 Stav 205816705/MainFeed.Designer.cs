46e41c639eedfd52a91b76ef67929219d88fe7bc
stavital <stavital7@gmail.com>
2020-11-11 18:35:05 +0200

    שינויים אפשריים

--- HW1/A21 Ex01 Omer 206126128 Stav 205816705/MainFeed.Designer.cs
+++ HW1/A21 Ex01 Omer 206126128 Stav 205816705/MainFeed.Designer.cs
@@ -34,30 +34,30 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.UserNamePictureBox = new System.Windows.Forms.PictureBox();
-            this.FeedBox = new System.Windows.Forms.ListBox();
             this.FetchaAlbumsBtn = new System.Windows.Forms.Button();
             this.FetchPostsBtn = new System.Windows.Forms.Button();
             this.FetchAccountInfoBtn = new System.Windows.Forms.Button();
             this.FetchEventsBtn = new System.Windows.Forms.Button();
-            this.Extra1 = new System.Windows.Forms.Button();
-            this.Extra2 = new System.Windows.Forms.Button();
-            this.AlbumPic1 = new System.Windows.Forms.PictureBox();
-            this.AlbumPic2 = new System.Windows.Forms.PictureBox();
+            this.Friends = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.NewPost = new System.Windows.Forms.RichTextBox();
+            this.FeedGroupBox = new System.Windows.Forms.GroupBox();
+            this.posted = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.UserNamePictureBox)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AlbumPic1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AlbumPic2)).BeginInit();
             this.SuspendLayout();
             // 
             // WelcomeUserNameLable
             // 
             this.WelcomeUserNameLable.AutoSize = true;
             this.WelcomeUserNameLable.BackColor = System.Drawing.Color.RoyalBlue;
-            this.WelcomeUserNameLable.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.WelcomeUserNameLable.Font = new System.Drawing.Font("Britannic Bold", 26F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.WelcomeUserNameLable.ForeColor = System.Drawing.Color.White;
             this.WelcomeUserNameLable.Location = new System.Drawing.Point(416, 9);
             this.WelcomeUserNameLable.Name = "WelcomeUserNameLable";
-            this.WelcomeUserNameLable.Size = new System.Drawing.Size(455, 55);
+            this.WelcomeUserNameLable.Size = new System.Drawing.Size(472, 58);
             this.WelcomeUserNameLable.TabIndex = 0;
             this.WelcomeUserNameLable.Text = "Welcome Full Name";
             // 
@@ -94,116 +94,160 @@
             this.UserNamePictureBox.TabIndex = 4;
             this.UserNamePictureBox.TabStop = false;
             // 
-            // FeedBox
-            // 
-            this.FeedBox.FormattingEnabled = true;
-            this.FeedBox.ItemHeight = 20;
-            this.FeedBox.Location = new System.Drawing.Point(344, 81);
-            this.FeedBox.Name = "FeedBox";
-            this.FeedBox.Size = new System.Drawing.Size(582, 624);
-            this.FeedBox.TabIndex = 6;
-            // 
             // FetchaAlbumsBtn
             // 
-            this.FetchaAlbumsBtn.Location = new System.Drawing.Point(43, 96);
+            this.FetchaAlbumsBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.FetchaAlbumsBtn.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FetchaAlbumsBtn.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FetchaAlbumsBtn.Location = new System.Drawing.Point(22, 256);
             this.FetchaAlbumsBtn.Name = "FetchaAlbumsBtn";
             this.FetchaAlbumsBtn.Size = new System.Drawing.Size(238, 45);
             this.FetchaAlbumsBtn.TabIndex = 7;
             this.FetchaAlbumsBtn.Text = "Albums";
-            this.FetchaAlbumsBtn.UseVisualStyleBackColor = true;
+            this.FetchaAlbumsBtn.UseVisualStyleBackColor = false;
             this.FetchaAlbumsBtn.Click += new System.EventHandler(this.FetchaAlbumsBtn_Click);
             // 
             // FetchPostsBtn
             // 
-            this.FetchPostsBtn.Location = new System.Drawing.Point(43, 159);
+            this.FetchPostsBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.FetchPostsBtn.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FetchPostsBtn.Location = new System.Drawing.Point(22, 150);
             this.FetchPostsBtn.Name = "FetchPostsBtn";
             this.FetchPostsBtn.Size = new System.Drawing.Size(238, 47);
             this.FetchPostsBtn.TabIndex = 8;
             this.FetchPostsBtn.Text = "Posts";
-            this.FetchPostsBtn.UseVisualStyleBackColor = true;
+            this.FetchPostsBtn.UseVisualStyleBackColor = false;
             this.FetchPostsBtn.Click += new System.EventHandler(this.FetchPostsBtn_Click);
             // 
             // FetchAccountInfoBtn
             // 
-            this.FetchAccountInfoBtn.Location = new System.Drawing.Point(43, 230);
+            this.FetchAccountInfoBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.FetchAccountInfoBtn.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FetchAccountInfoBtn.Location = new System.Drawing.Point(22, 97);
             this.FetchAccountInfoBtn.Name = "FetchAccountInfoBtn";
             this.FetchAccountInfoBtn.Size = new System.Drawing.Size(238, 47);
             this.FetchAccountInfoBtn.TabIndex = 9;
             this.FetchAccountInfoBtn.Text = "Account";
-            this.FetchAccountInfoBtn.UseVisualStyleBackColor = true;
+            this.FetchAccountInfoBtn.UseVisualStyleBackColor = false;
             this.FetchAccountInfoBtn.Click += new System.EventHandler(this.FetchAccountInfoBtn_Click);
             // 
             // FetchEventsBtn
             // 
-            this.FetchEventsBtn.Location = new System.Drawing.Point(43, 298);
+            this.FetchEventsBtn.BackColor = System.Drawing.Color.RoyalBlue;
+            this.FetchEventsBtn.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FetchEventsBtn.Location = new System.Drawing.Point(22, 307);
             this.FetchEventsBtn.Name = "FetchEventsBtn";
             this.FetchEventsBtn.Size = new System.Drawing.Size(238, 47);
             this.FetchEventsBtn.TabIndex = 10;
             this.FetchEventsBtn.Text = "Events";
-            this.FetchEventsBtn.UseVisualStyleBackColor = true;
+            this.FetchEventsBtn.UseVisualStyleBackColor = false;
             this.FetchEventsBtn.Click += new System.EventHandler(this.FetchEventsBtn_Click);
             // 
-            // Extra1
+            // Friends
             // 
-            this.Extra1.Location = new System.Drawing.Point(43, 369);
-            this.Extra1.Name = "Extra1";
-            this.Extra1.Size = new System.Drawing.Size(238, 47);
-            this.Extra1.TabIndex = 11;
-            this.Extra1.Text = "Extra";
-            this.Extra1.UseVisualStyleBackColor = true;
+            this.Friends.BackColor = System.Drawing.Color.RoyalBlue;
+            this.Friends.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Friends.Location = new System.Drawing.Point(22, 203);
+            this.Friends.Name = "Friends";
+            this.Friends.Size = new System.Drawing.Size(238, 47);
+            this.Friends.TabIndex = 12;
+            this.Friends.Text = "Friends";
+            this.Friends.UseVisualStyleBackColor = false;
             // 
-            // Extra2
+            // button1
             // 
-            this.Extra2.Location = new System.Drawing.Point(43, 437);
-            this.Extra2.Name = "Extra2";
-            this.Extra2.Size = new System.Drawing.Size(238, 47);
-            this.Extra2.TabIndex = 12;
-            this.Extra2.Text = "Extra";
-            this.Extra2.UseVisualStyleBackColor = true;
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 360);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(238, 47);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
             // 
-            // AlbumPic1
+            // button2
             // 
-            this.AlbumPic1.Location = new System.Drawing.Point(803, 93);
-            this.AlbumPic1.Name = "AlbumPic1";
-            this.AlbumPic1.Size = new System.Drawing.Size(101, 82);
-            this.AlbumPic1.TabIndex = 13;
-            this.AlbumPic1.TabStop = false;
-            this.AlbumPic1.Visible = false;
+            this.button2.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button2.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(22, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(238, 47);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Chat";
+            this.button2.UseVisualStyleBackColor = false;
             // 
-            // AlbumPic2
+            // button3
             // 
-            this.AlbumPic2.Location = new System.Drawing.Point(803, 195);
-            this.AlbumPic2.Name = "AlbumPic2";
-            this.AlbumPic2.Size = new System.Drawing.Size(101, 93);
-            this.AlbumPic2.TabIndex = 14;
-            this.AlbumPic2.TabStop = false;
-            this.AlbumPic2.Visible = false;
+            this.button3.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button3.Font = new System.Drawing.Font("Britannic Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(22, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(238, 47);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Home";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // NewPost
+            // 
+            this.NewPost.Font = new System.Drawing.Font("Berlin Sans FB", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewPost.Location = new System.Drawing.Point(276, 101);
+            this.NewPost.Name = "NewPost";
+            this.NewPost.Size = new System.Drawing.Size(693, 111);
+            this.NewPost.TabIndex = 18;
+            this.NewPost.Text = "What\'s on your mind?";
+            this.NewPost.TextChanged += new System.EventHandler(this.CreatNewPost);
+            // 
+            // FeedGroupBox
+            // 
+            this.FeedGroupBox.AutoSize = true;
+            this.FeedGroupBox.BackColor = System.Drawing.Color.White;
+            this.FeedGroupBox.Font = new System.Drawing.Font("Berlin Sans FB", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FeedGroupBox.Location = new System.Drawing.Point(276, 256);
+            this.FeedGroupBox.Name = "FeedGroupBox";
+            this.FeedGroupBox.Size = new System.Drawing.Size(693, 411);
+            this.FeedGroupBox.TabIndex = 20;
+            this.FeedGroupBox.TabStop = false;
+            // 
+            // posted
+            // 
+            this.posted.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.posted.Font = new System.Drawing.Font("Britannic Bold", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.posted.Location = new System.Drawing.Point(878, 215);
+            this.posted.Name = "posted";
+            this.posted.Size = new System.Drawing.Size(91, 35);
+            this.posted.TabIndex = 19;
+            this.posted.Text = "Posted";
+            this.posted.UseVisualStyleBackColor = false;
+            this.posted.Visible = false;
             // 
             // MainFeed
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(144F, 144F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.ClientSize = new System.Drawing.Size(1303, 692);
-            this.Controls.Add(this.AlbumPic2);
-            this.Controls.Add(this.AlbumPic1);
-            this.Controls.Add(this.Extra2);
-            this.Controls.Add(this.Extra1);
+            this.Controls.Add(this.FeedGroupBox);
+            this.Controls.Add(this.posted);
+            this.Controls.Add(this.NewPost);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Friends);
             this.Controls.Add(this.FetchEventsBtn);
             this.Controls.Add(this.FetchAccountInfoBtn);
             this.Controls.Add(this.FetchPostsBtn);
             this.Controls.Add(this.FetchaAlbumsBtn);
-            this.Controls.Add(this.FeedBox);
             this.Controls.Add(this.UserNamePictureBox);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.WelcomeUserNameLable);
             this.Controls.Add(this.label1);
             this.Name = "MainFeed";
-            this.Text = "Form1";
+            this.Text = "newPost";
+            this.Click += new System.EventHandler(this.FetchaAlbumsBtn_Click);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.UserNamePictureBox)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AlbumPic1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.AlbumPic2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -216,14 +260,16 @@
         private System.Windows.Forms.ImageList imageList1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox UserNamePictureBox;
-        private System.Windows.Forms.ListBox FeedBox;
         private System.Windows.Forms.Button FetchaAlbumsBtn;
         private System.Windows.Forms.Button FetchPostsBtn;
         private System.Windows.Forms.Button FetchAccountInfoBtn;
         private System.Windows.Forms.Button FetchEventsBtn;
-        private System.Windows.Forms.Button Extra1;
-        private System.Windows.Forms.Button Extra2;
-        private System.Windows.Forms.PictureBox AlbumPic1;
-        private System.Windows.Forms.PictureBox AlbumPic2;
+        private System.Windows.Forms.Button Friends;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox NewPost;
+        private System.Windows.Forms.GroupBox FeedGroupBox;
+        private System.Windows.Forms.Button posted;
     }
 }
--- HW1/A21 Ex01 Omer 206126128 Stav 205816705/MainFeed.cs
+++ HW1/A21 Ex01 Omer 206126128 Stav 205816705/MainFeed.cs
@@ -24,58 +24,111 @@ namespace A21_Ex01_Omer_206126128_Stav_205816705
         {
             WelcomeUserNameLable.Text = "Welcome " + Global.User.Name;
             UserNamePictureBox.Image = Global.User.ImageSmall;
-            FeedBox.Items.Add("Please click on a button");
         }
 
         private void FetchaAlbumsBtn_Click(object sender, EventArgs e)
         {
-            FeedBox.Items.Clear();
+            FeedGroupBox.Visible = true;
+            FeedGroupBox.Controls.Clear();
+
             FacebookObjectCollection<Album> albums = Global.User.Albums;
-            int i = 0; 
+            Point picLocation = new Point(20, 50);
             foreach (Album album in albums)
             {
                 PictureBox albumPicture = new PictureBox();
-                albumPicture.Location = new System.Drawing.Point(250,30+i);
-                albumPicture.Size = new System.Drawing.Size(75, 75);
+                albumPicture.Size = new System.Drawing.Size(150, 150);
+                albumPicture.Location = picLocation;
                 albumPicture.SizeMode = PictureBoxSizeMode.StretchImage;
                 albumPicture.LoadAsync(album.PictureAlbumURL);
-                FeedBox.Items.Add(album.Name + "(" + album.Count + " Likes)");
-                FeedBox.Controls.Add(albumPicture);
-                FeedBox.Items.Add("------------------------------------------------------");
-                FeedBox.Items.Add("");
-                FeedBox.Items.Add("");
-                FeedBox.Items.Add("");
-                i += 100;
+                FeedGroupBox.Controls.Add(albumPicture);
+
+                Label albumName = new Label();
+                albumName.Text = album.Name + "(" + album.Count + " Likes)";
+                albumName.ForeColor = Color.Black;
+                albumName.Location = new Point(picLocation.X, picLocation.Y - albumName.Size.Height);
+                FeedGroupBox.Controls.Add(albumName);
+
+                picLocation = calculateNextAlbumCUverPhotoPosition(picLocation);
             }
+        }
+
+        private Point calculateNextAlbumCUverPhotoPosition(Point i_prevPoint)
+        {
+            int x = i_prevPoint.X;
+            int y = i_prevPoint.Y;
+
+            x = (x + 200) % 400;
+            if (x <= 50)
+            {
+                y += 220;
+            }
+
+            return new Point(x, y);
         }
 
         private void FetchPostsBtn_Click(object sender, EventArgs e)
         {
-            FeedBox.Items.Clear();
+            FeedGroupBox.Controls.Clear();
+
+            Point labelLocation = new Point(20, 10);
+            int Y_Offset;
             foreach (Post post in Global.User.Posts)
             {
-                FeedBox.Items.Add(post.From.Name);
+                Label postFromName = new Label();
+                postFromName.Text = post.From.Name;
+                postFromName.Location = labelLocation;
+                FeedGroupBox.Controls.Add(postFromName);
+
+                if (!postFromName.Text.Equals(Global.User.Name))
+                {
+                    Label ToMyUser = new Label();
+                    ToMyUser.Text = "->" + Global.User.Name;
+                    ToMyUser.Location = new Point(labelLocation.X + postFromName.Width, labelLocation.Y);
+                    FeedGroupBox.Controls.Add(ToMyUser);
+                }
+
+                Y_Offset = 1;
                 if (post.Name != null)
                 {
-                    FeedBox.Items.Add(post.Name);
+                    Label postName = new Label();
+                    postName.Text = post.Name;
+                    postName.Location = new Point(labelLocation.X, labelLocation.Y + Y_Offset * postFromName.Height);
+                    Y_Offset++;
+                    FeedGroupBox.Controls.Add(postName);
                 }
-                FeedBox.Items.Add(post.CreatedTime);
-                FeedBox.Items.Add("");
+
+                if (post.Message != null)
+                {
+                    Label postMessage = new Label();
+                    postMessage.Text = post.Message;
+                    postMessage.Location = new Point(labelLocation.X, labelLocation.Y + Y_Offset * postFromName.Height);
+                    FeedGroupBox.Controls.Add(postMessage);
+                }
+
+                labelLocation = calculateNextPostPosition(labelLocation);
             }
+        }
+
+        private Point calculateNextPostPosition(Point i_prevPoint)
+        {
+            return new Point(i_prevPoint.X, i_prevPoint.Y + 80);
         }
 
         private void FetchAccountInfoBtn_Click(object sender, EventArgs e)
         {
+            /*
             FeedBox.Items.Clear();
             FeedBox.Items.Add("Name : " + Global.User.Name);
             FeedBox.Items.Add("Email : " + Global.User.Email);
             FeedBox.Items.Add("Birthday : " + Global.User.Birthday);
             FeedBox.Items.Add("Gender : " + Global.User.Gender);
             FeedBox.Items.Add("Home Town:" + Global.User.Hometown);
+            */
         }
 
         private void FetchEventsBtn_Click(object sender, EventArgs e)
         {
+            /*
             FeedBox.Items.Clear();
             foreach (Event event_ in Global.User.Events)
             {
@@ -83,6 +136,13 @@ namespace A21_Ex01_Omer_206126128_Stav_205816705
                 FeedBox.Items.Add(event_.StartTime);
                 FeedBox.Items.Add("");
             }
+            */
         }
+
+        private void CreatNewPost(object sender, EventArgs e)
+        {
+            
+        }
+
     }
 }
